5bde85976864052b5575c096a371ed8ccd618a93
Gareth Fuller <gareth.fuller@clark.de>
2016-12-04 22:33:33 +0100

    Scoreboard app (react basics course)

---
+++ scoreboard/webpack.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  entry: './app.js',
+  
+  output: {
+    // path: 'build',
+    filename: 'bundle.js'
+  },
+  
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader' 
+      }
+    ]
+  }
+};
