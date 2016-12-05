d92cdec050a12189723bfb3d82cd0e8ef37905f2
Gareth Fuller <gareth.fuller@clark.de>
2016-12-05 23:28:40 +0100

    Adding even more routes

--- router/src/router.js
+++ router/src/router.js
@@ -6,6 +6,9 @@ import Home from './components/Home';
 import About from './components/About';
 import Teachers from './components/Teachers';
 import Courses from './components/Courses';
+import HTML from './components/courses/HTML';
+import CSS from './components/courses/CSS';
+import JavaScript from './components/courses/JavaScript';
 
 const routes = (
   <Router history={browserHistory}>
@@ -13,7 +16,11 @@ const routes = (
       <Route path="/" component={Home}/>
       <Route path="about" component={About}/>
       <Route path="teachers" component={Teachers}/>
-      <Route path="courses" component={Courses}/>
+      <Route path="courses" component={Courses}>
+        <Route path="html" component={HTML} />
+        <Route path="css" component={CSS} />
+        <Route path="javascript" component={JavaScript} />
+      </Route>
     </Route>
   </Router>
 );
