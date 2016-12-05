e862f4a6cd0f1d352c727170b41b80c40172b494
Gareth Fuller <gareth.fuller@clark.de>
2016-12-05 23:06:26 +0100

    Move routes into new file

--- router/src/index.js
+++ router/src/index.js
@@ -1,28 +1,9 @@
-// Libs
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import routes from './router';
 
 // CSS
 import './css/style.css';
 
-// Components
-import App from './components/App';
-import Home from './components/Home';
-import About from './components/About';
-import Teachers from './components/Teachers';
-import Courses from './components/Courses';
-
 // Render
-render((
-    <Router history={hashHistory}>
-      <Route component={App}>
-        <Route path="/" component={Home}/>
-        <Route path="about" component={About}/>
-        <Route path="teachers" component={Teachers}/>
-        <Route path="courses" component={Courses}/>
-      </Route>
-    </Router>
-  ),
-  document.getElementById('root')
-);
+render(routes, document.getElementById('root'));
---
+++ router/src/router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
+
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+import Teachers from './components/Teachers';
+import Courses from './components/Courses';
+
+const routes = (
+  <Router history={hashHistory}>
+    <Route component={App}>
+      <Route path="/" component={Home}/>
+      <Route path="about" component={About}/>
+      <Route path="teachers" component={Teachers}/>
+      <Route path="courses" component={Courses}/>
+    </Route>
+  </Router>
+);
+
+export default routes;
