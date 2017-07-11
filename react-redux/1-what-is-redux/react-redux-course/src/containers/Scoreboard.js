f68d7a91b5d467a53b2bcffc0240b2a388b06cb1
Gareth Fuller <gareth.fuller@clark.de>
2017-07-11 21:31:56 +0200

    modularize the app, in prep for redux baby!

--- react-redux/1-what-is-redux/react-redux-course/src/containers/Scoreboard.js
+++ react-redux/1-what-is-redux/react-redux-course/src/containers/Scoreboard.js
@@ -1,45 +1,43 @@
-import React from 'react';
-import Stopwatch from '../components/Stopwatch';
-import Stats from '../components/Stats';
-import Counter from '../components/Counter';
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Player from '../components/Player';
+import AddPlayerForm from '../components/AddPlayerForm';
 
-const INITIAL_STATE = {
-  players: [
-    {
-      name: 'Jim Hoskins',
-      score: 31,
-    },
-    {
-      name: 'Andrew Chalkley',
-      score: 20,
-    },
-    {
-      name: 'Alena Holligan',
-      score: 50,
-    },
-  ],
-}
 
-const Scoreboard = React.createClass({
-  getInitialState: function () {
-    return INITIAL_STATE;
-  },
-  onScoreChange: function(index, delta) {
+export default class Scoreboard extends Component {
+  state = {
+      players: [
+      {
+        name: 'Jim Hoskins',
+        score: 31,
+      },
+      {
+        name: 'Andrew Chalkley',
+        score: 20,
+      },
+      {
+        name: 'Alena Holligan',
+        score: 50,
+      },
+    ]
+  };
+
+  onScoreChange = (index, delta) => {
     this.state.players[index].score += delta;
     this.setState(this.state);
-  },
+  };
 
-  onAddPlayer: function(name) {
+  onAddPlayer = (name) =>  {
     this.state.players.push({ name: name, score: 0 });
     this.setState(this.state);
-  },
+  };
 
-  onRemovePlayer: function(index) {
+  onRemovePlayer = (index) => {
     this.state.players.splice(index, 1);
     this.setState(this.state);
-  },
+  };
 
-  render: function() {
+  render() {
     return (
       <div className="scoreboard">
         <Header players={this.state.players} />
@@ -59,84 +57,5 @@ const Scoreboard = React.createClass({
         <AddPlayerForm onAdd={this.onAddPlayer} />
       </div>
     );
-  }
-});
-
-// ----------------------------------------------
-
-function Header(props) {
-  return (
-    <div className="header">
-      <Stats players={props.players} />
-      <h1>Scoreboard</h1>
-      <Stopwatch />
-    </div>
-  );
+  };
 }
-
-Header.propTypes = {
-  players: React.PropTypes.array.isRequired,
-};
-
-
-function Player(props) {
-  return (
-    <div className="player">
-      <div className="player-name">
-        <a className="remove-player" onClick={props.onRemove}>✖</a>
-        {props.name}
-      </div>
-      <div className="player-score">
-        <Counter onChange={props.onScoreChange} score={props.score} />
-      </div>
-    </div>
-  );
-}
-
-Player.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number.isRequired,
-  onRemove: React.PropTypes.func.isRequired,
-  onScoreChange: React.PropTypes.func.isRequired,
-};
-
-
-const AddPlayerForm = React.createClass({
-  propTypes: {
-    onAdd: React.PropTypes.func.isRequired,
-  },
-
-  getInitialState: function () {
-    return { name: '' };
-  },
-
-  onNameChange: function (e) {
-    const name = e.target.value;
-    this.setState({ name: name });
-  },
-
-  onSubmit: function (e) {
-    if (e) e.preventDefault();
-    this.props.onAdd(this.state.name);
-    this.setState({ name: '' });
-  },
-
-  render: function () {
-    return (
-      <div className="add-player-form">
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.onNameChange}
-            placeholder="Player Name"
-          />
-          <input type="submit" value="Add Player" />
-        </form>
-      </div>
-    );
-  }
-});
-
-
-export default Scoreboard;
