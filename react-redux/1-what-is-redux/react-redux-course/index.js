592f1f3b9e04d567c84a0566fa2860637e79ecda
Gareth Fuller <gareth.fuller@clark.de>
2017-07-13 22:51:55 +0200

    hooking up redux baby!

--- react-redux/1-what-is-redux/react-redux-course/index.js
+++ react-redux/1-what-is-redux/react-redux-course/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/player.js';
 import Scoreboard from './src/containers/Scoreboard';
 
+// lets create the store!
+// register the reducer with redux
+const store = createStore(
+  PlayerReducer
+);
+
+
+// wrap the app in provider, so the score board has access to method
+// to listed to events
 render(
-  <Scoreboard/>,
+  <Provider store={store}>
+    <Scoreboard/>
+  </Provider>,
   document.getElementById('root')
 );
--- react-redux/1-what-is-redux/react-redux-course/src/containers/Scoreboard.js
+++ react-redux/1-what-is-redux/react-redux-course/src/containers/Scoreboard.js
@@ -1,48 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as PlayerActionCreators from '../actions/players';
 import Header from '../components/Header';
 import Player from '../components/Player';
 import AddPlayerForm from '../components/AddPlayerForm';
 
-
-export default class Scoreboard extends Component {
-  state = {
-    players: []
-  };
-
-  onScoreChange = (index, delta) => {
-    this.state.players[index].score += delta;
-    this.setState(this.state);
-  };
-
-  onAddPlayer = (name) =>  {
-    this.state.players.push({ name: name, score: 0 });
-    this.setState(this.state);
-  };
-
-  onRemovePlayer = (index) => {
-    this.state.players.splice(index, 1);
-    this.setState(this.state);
+class Scoreboard extends Component {
+  static PropTypes = {
+    players: PropTypes.array.isRequired
   };
 
   render() {
+    const { dispatch, players } = this.props;
+
+    // action creators
+    // bind action creators, take dispatch as second arg. makes sure when invoked
+    // is also dispatched
+    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
+    const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
+
+    // iterate over players and create a player component for each player
+    const playerComponents = players.map((player, index) => (
+      <Player
+        index={index}
+        name={player.name}
+        score={player.score}
+        key={player.name}
+        updatePlayerScore={updatePlayerScore}
+        removePlayer={removePlayer}
+      />
+    ));
+
     return (
       <div className="scoreboard">
-        <Header players={this.state.players} />
+        <Header players={players} />
         <div className="players">
-          {this.state.players.map(function(player, index) {
-             return (
-               <Player
-                 name={player.name}
-                 score={player.score}
-                 key={player.name}
-                 onScoreChange={(delta) => this.onScoreChange(index, delta)}
-                 onRemove={() => this.onRemovePlayer(index)}
-               />
-             );
-           }.bind(this))}
+          { playerComponents }
         </div>
-        <AddPlayerForm onAdd={this.onAddPlayer} />
+        <AddPlayerForm addPlayer={addPlayer} />
       </div>
     );
-  };
+  }
 }
+
+const mapStateToProps = state => ({
+  players: state
+});
+
+// first set is function we want to use to transform state to props
+// second is container we want to connect to redux
+// subscribe storeboard to any state changes and when it happen the function
+// 'mapStateToProps' is called. this will then map players
+// the double () is because we are calling a the evaluated function returned from connect
+export default connect(mapStateToProps)(Scoreboard);
