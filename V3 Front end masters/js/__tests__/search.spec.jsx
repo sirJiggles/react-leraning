65e16a04a535bf916ca74584b0e0ed38668bc422
Gareth Fuller <gareth.fuller@clark.de>
2018-03-29 00:26:47 +0200

    moar tests

--- V3 Front end masters/js/__tests__/search.spec.jsx
+++ V3 Front end masters/js/__tests__/search.spec.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Search from '../Search';
+import ShowCard from '../ShowCard';
+import data from '../../data.json';
 
 test('Search renders correctly', () => {
   // shallow will stub out child components
@@ -9,4 +11,20 @@ test('Search renders correctly', () => {
 
   // should match the snapshot that is generated!
   expect(component).toMatchSnapshot();
+});
+
+test('Search should remember correct amount of shows', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(data.shows.length);
+});
+
+test('Search should render correct based on search term', () => {
+  const searchWord = 'black';
+  const component = shallow(<Search />);
+  component.find('input').simulate('change', { target: { value: searchWord } });
+  // Ideally rather than the hard set 2, could move the filter and so on into a module
+  // that takes a term arg, then input the module here and in the usage
+  // then deirve the number in here. this means if the data changes we dont need
+  // to update the tests
+  expect(component.find(ShowCard).length).toEqual(2);
 });
