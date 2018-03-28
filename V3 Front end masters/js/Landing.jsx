d2edd5053ef9af8aa4260b84a45ca301f3259fd5
Gareth Fuller <gareth.fuller@clark.de>
2018-03-28 19:50:42 +0200

    playing with hash router

--- V3 Front end masters/js/Landing.jsx
+++ V3 Front end masters/js/Landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div className="landing">
     <h1>svideo</h1>
     <input type="text" placeholder="Search" />
-    <a>Browse all</a>
+    <Link to="/search">Browse all</Link>
   </div>
 );
 
---
+++ V3 Front end masters/js/Search.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Search = () => <h1>This is a silly thing</h1>;
+
+export default Search;
