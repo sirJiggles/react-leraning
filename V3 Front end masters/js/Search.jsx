32b72f984e2c04f3c1a9f709992725e8bf824850
Gareth Fuller <gareth.fuller@clark.de>
2018-03-28 20:07:01 +0200

    showing some titles in our search

--- V3 Front end masters/js/Search.jsx
+++ V3 Front end masters/js/Search.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import preload from '../data.json';
 
-const Search = () => <h1>This is a silly thing</h1>;
+const Search = () => (
+  <div className="search">
+    {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
+    {preload.shows.map(show => <h3>{show.title}</h3>)}
+  </div>
+);
 
 export default Search;
