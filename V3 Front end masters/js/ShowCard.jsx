d758c40bd6b91fe88a091cad7c9cd406f3eaeec8
Gareth Fuller <gareth.fuller@clark.de>
2018-03-28 21:37:02 +0200

    add prop types for the show card, and prop validation

--- V3 Front end masters/js/ShowCard.jsx
+++ V3 Front end masters/js/ShowCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shape, string } from 'prop-types';
 
 const ShowCard = props => (
   <div className="show-card">
@@ -6,9 +7,18 @@ const ShowCard = props => (
     <div>
       <h3>{props.show.title}</h3>
       <h4>({props.show.year})</h4>
-      <p>{props.show.decription}</p>
+      <p>{props.show.description}</p>
     </div>
   </div>
 );
+
+ShowCard.propTypes = {
+  show: shape({
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+  }).isRequired
+};
 
 export default ShowCard;
