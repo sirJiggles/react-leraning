e625d1ab44c82fa56f9b11a993ce445d4cea649f
Gareth Fuller <gareth.fuller@clark.de>
2018-03-28 22:13:52 +0200

    styled components ... very cool!

--- V3 Front end masters/js/Search.jsx
+++ V3 Front end masters/js/Search.jsx
@@ -5,7 +5,9 @@ import ShowCard from './ShowCard';
 const Search = () => (
   <div className="search">
     {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
-    {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+    <div>
+      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
   </div>
 );
 
--- V3 Front end masters/js/ShowCard.jsx
+++ V3 Front end masters/js/ShowCard.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { string } from 'prop-types';
+import styled from 'styled-components';
+
+// Tagged template literal
+// the styled is a function this func returns a div that
+// is styled like this, this is a react component
+// http://wesbos.com/tagged-template-literals/
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
 
 const ShowCard = props => (
-  <div className="show-card">
-    <img src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
+  <Wrapper>
+    <Image src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
     <div>
       <h3>{props.title}</h3>
       <h4>({props.year})</h4>
       <p>{props.description}</p>
     </div>
-  </div>
+  </Wrapper>
 );
 
 ShowCard.propTypes = {
