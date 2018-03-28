7e242c40f57f8464ed954d7c3084842129bac3a0
Gareth Fuller <gareth.fuller@clark.de>
2018-03-28 20:20:28 +0200

    more sub components

--- V3 Front end masters/js/Search.jsx
+++ V3 Front end masters/js/Search.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import preload from '../data.json';
+import ShowCard from './ShowCard';
 
 const Search = () => (
   <div className="search">
     {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
-    {preload.shows.map(show => <h3>{show.title}</h3>)}
+    {preload.shows.map(show => <ShowCard show={show} />)}
   </div>
 );
 
