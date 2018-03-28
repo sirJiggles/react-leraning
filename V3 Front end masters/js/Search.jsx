76fbcde54bcbb69ed1d4e34c40ca52ad1be931f0
Gareth Fuller <gareth.fuller@clark.de>
2018-03-28 21:42:43 +0200

    spread on object to make props smaller

--- V3 Front end masters/js/Search.jsx
+++ V3 Front end masters/js/Search.jsx
@@ -5,7 +5,7 @@ import ShowCard from './ShowCard';
 const Search = () => (
   <div className="search">
     {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
-    {preload.shows.map(show => <ShowCard show={show} />)}
+    {preload.shows.map(show => <ShowCard {...show} />)}
   </div>
 );
 
