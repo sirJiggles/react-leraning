7a7ad3815003d1d5a33205d0bf9e027359ea1237
Gareth Fuller <gareth.fuller@clark.de>
2018-03-28 19:29:59 +0200

    add lint hook in webback and start new app

--- V3 Front end masters/js/clientApp.jsx
+++ V3 Front end masters/js/clientApp.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const myTitle = function myTitle(props) {
-  return (
-    <div>
-      <h1 style={{ color: props.color }}>{props.title}</h1>
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>Browse all</a>
     </div>
-  );
-};
+  </div>
+);
 
-// composit component "comoonent of components"
-const myFirstComponent = function myFirstComponent() {
-  return (
-    <div id="my-sample-component">
-      <myTitle color="YellowGreen" title="Game of thrones" />
-      <myTitle color="GreenYellow" title="Stranger things" />
-      <myTitle color="peru" title="Rick and morty" />
-    </div>
-  );
-};
-
-render(<myFirstComponent />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
