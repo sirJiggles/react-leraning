946fc98e9a73e7ee10844973edb5b0b9fd47f777
Gareth Fuller <gareth.fuller@clark.de>
2018-04-07 11:26:15 +0200

    webpack fixe dagain :D

---
+++ V3 Front end masters/dist/Details.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+declare const Detailts: () => JSX.Element;
+export default Detailts;
---
+++ V3 Front end masters/js/Details.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Detailts = () => (
+  <div className="details">
+    <h1>Hi</h1>
+  </div>
+);
+
+export default Detailts;
