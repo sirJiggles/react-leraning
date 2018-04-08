8b4858956eea9f43856b830c3bf8c58acf06eb5b
Gareth Fuller <gareth.fuller@clark.de>
2018-04-08 22:06:21 +0200

    Add link on search page

--- V3 Front end masters/js/ShowCard.tsx
+++ V3 Front end masters/js/ShowCard.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import InterfaceShow from './interfaces/Show';
+import { Link } from 'react-router-dom';
 
 // Tagged template literal
 // the styled is a function this func returns a div that
 // is styled like this, this is a react component
 // http://wesbos.com/tagged-template-literals/
-const Wrapper = styled.div`
+const Wrapper = styled(Link)`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -22,7 +25,7 @@ const Image = styled.img`
 `;
 
 const ShowCard = (props: InterfaceShow) => (
-  <Wrapper>
+  <Wrapper to={`/details/${props.imdbID}`}>
     <Image src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
     <div>
       <h3>{props.title}</h3>
