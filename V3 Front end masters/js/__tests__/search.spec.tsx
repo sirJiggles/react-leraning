ed1b166cbf4d4e9e678fb1017da8ba68d43d1d94
Gareth Fuller <gareth.fuller@clark.de>
2018-04-08 18:38:56 +0200

    working on configuring jest / enzyme with TS, this configuration nonesense is a friking joke what has the world of JS come too ...

--- V3 Front end masters/js/__tests__/search.spec.tsx
+++ V3 Front end masters/js/__tests__/search.spec.tsx
@@ -7,20 +7,20 @@ import data from '../data';
 test('Search renders correctly', () => {
   // shallow will stub out child components
   // so if tests fail in child items, they wont fail up here
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={data.shows} />);
 
   // should match the snapshot that is generated!
   expect(component).toMatchSnapshot();
 });
 
 test('Search should remember correct amount of shows', () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={data.shows} />);
   expect(component.find(ShowCard).length).toEqual(data.shows.length);
 });
 
 test('Search should render correct based on search term', () => {
   const searchWord = 'black';
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={data.shows} />);
   component.find('input').simulate('change', { target: { value: searchWord } });
   // Ideally rather than the hard set 2, could move the filter and so on into a module
   // that takes a term arg, then input the module here and in the usage
