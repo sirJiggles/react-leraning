c8d2c7ede5ef30b405edbbc23ac36f7e9316eead
Gareth Fuller <garethfuller@Gareths-MBP.home>
2018-05-14 23:46:07 +0100

    add the thunk function into the action creators file

--- V3 Front end masters/js/action-creators.ts
+++ V3 Front end masters/js/action-creators.ts
@@ -1,6 +1,7 @@
 import { InterfaceAction, InterfaceShowAction } from './interfaces/Action';
 import Actions from './enums/Actions';
 import InterfaceShow from './interfaces/Show';
+import axios from 'axios';
 
 export function setSearchTerm(searchTerm: string): InterfaceAction {
   return {
@@ -13,5 +14,23 @@ export function addAPIData(apiData: InterfaceShow): InterfaceShowAction {
   return {
     type: Actions.ADD_API_DATA,
     payload: apiData
+  };
+}
+
+// this is the one that returns a thunk aka function, so this is a defered action
+// I will wait for dispatch
+export function getAPIDetails(imdbID: string) {
+  // this function is the "thunk"
+  // this is what will be invoked by the middleware we added using redux thunk
+  return (dispatch: Function) => {
+    axios
+      .get(`http://localhost:3000/${imdbID}`)
+      .then(response => {
+        // now resolve and give the action with the data from the API req
+        dispatch(addAPIData(response.data));
+      })
+      .catch(err => {
+        console.error('axios erorr', err);
+      });
   };
 }
