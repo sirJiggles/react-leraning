57bbe50199c37c0d64c6242e2f0546aa22ec41a9
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2018-05-16 19:05:55 +0100

    testing reducers using redux dev tools

---
+++ V3 Front end masters/js/__tests__/unit/reducers.spec.ts
@@ -0,0 +1,41 @@
+import reducers from '../../reducers';
+
+// pasted from redux dev tools!
+test('setSearchTerm', () => {
+  let state;
+  state = reducers({ searchTerm: '', apiData: {} }, { type: 'setSearchTerm', payload: 'black' });
+  expect(state).toEqual({ searchTerm: 'black', apiData: {} });
+});
+
+test('addAPIData', () => {
+  let state;
+  state = reducers(
+    { searchTerm: '', apiData: {} },
+    {
+      type: 'addAPIData',
+      payload: {
+        rating: '7.8',
+        title: 'Westworld',
+        year: '2016–',
+        description: 'Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.',
+        poster: 'ww.jpg',
+        imdbID: 'tt0475784',
+        trailer: 'eX3u0IlBBO4'
+      }
+    }
+  );
+  expect(state).toEqual({
+    searchTerm: '',
+    apiData: {
+      tt0475784: {
+        rating: '7.8',
+        title: 'Westworld',
+        year: '2016–',
+        description: 'Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.',
+        poster: 'ww.jpg',
+        imdbID: 'tt0475784',
+        trailer: 'eX3u0IlBBO4'
+      }
+    }
+  });
+});
